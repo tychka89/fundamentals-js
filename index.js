69fb585ff0393de2d2810519585645e726f3472e
Nataliya Rodikova <bachininang@gmail.com>
2023-12-11 01:37:14 +0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+export const concatStrings = (a, b) => {
+    return (a + b);
+};
 
+export const isString = (str) => {
+    return typeof str === 'string';
+};
+
+export const identifySign = (num) => {
+    if (num === 0) {
+        return 'Ноль';
+    } else if (num > 0) {
+        return 'Положительное число';
+    } else {
+        return 'Отрицательное число';
+    }
+};
+
+export const reverseWords = (text) => {
+    return text.split(' ').reverse().join(' ')
+};
+
+export const wordsCount = (text) => {
+    if (text === '') {
+        return 0;
+    } return text.split(' ').length;
+};
+
+
+console.log(wordsCount('Hexlet Hello'))
